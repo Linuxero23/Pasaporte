3283c902e74abde688ab677afafd7879d7131339
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-08-27 21:17:08 -0500

    Update Main.java

--- src/co/poli/edu/actividad1/vista/Main.java
+++ src/co/poli/edu/actividad1/vista/Main.java
@@ -1,85 +1,38 @@
 package co.poli.edu.actividad1.vista;
+import java.util.*;
 
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import co.poli.edu.actividad1.modelo.Pasaporte;
 import co.poli.edu.actividad1.repositorio.PasaporteRepositorio;
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-public class Main extends Application {
-    private PasaporteRepositorio repositorio = new PasaporteRepositorio();
+import co.poli.edu.actividad1.modelo.Titular;
+import co.poli.edu.actividad1.modelo.Ciudad;
+import co.poli.edu.actividad1.modelo.Pais;
 
-
-    @Override
-    public void start(Stage primaryStage) {
-        mostrarMenuPrincipal(primaryStage);
+public class Main  {
+    private static PasaporteRepositorio repositorio = new PasaporteRepositorio();
+    public static void main(String[] args) throws InterruptedException {
+        List<Ciudad> lst_ciudades=new ArrayList<>();
+        lst_ciudades.add(new Ciudad("1","Bogotá"));
+        lst_ciudades.add(new Ciudad("2","Chiquinquira"));
+        lst_ciudades.add(new Ciudad("3","Granada"));
+        lst_ciudades.add(new Ciudad("4","Mariquita"));
+        Pais Colombia=new Pais("1","Colombia",lst_ciudades);
+        Titular marg=new Titular("1","Margaret Corasick","13/05/1945");
+        Pasaporte pasaporte=new Pasaporte("1","27/08/2035",marg, Colombia);
+        List<Pasaporte> s=seleccionarTodos();
+        for(Pasaporte p:s)
+            System.out.println(p);
+    }
+    static String insertar(Pasaporte pasaporte) {
+        return repositorio.insertar(pasaporte);
+    }
+    static String eliminar(String id){
+        return repositorio.eliminar(id);
+    }
+    static Pasaporte seleccionar(String id){
+        return repositorio.seleccionar(id);
+    }
+    static List<Pasaporte> seleccionarTodos(){
+        return repositorio.seleccionarTodos();
     }
 
-    private void mostrarMenuPrincipal(Stage primaryStage) {
-        Label titulo = new Label("Menú Principal - Gestión de Pasaportes");
-
-        Button btnInsertar = new Button("Insertar");
-        btnInsertar.setOnAction(e -> insertar(primaryStage));
-
-        Button btnEliminar = new Button("Eliminar");
-        //btnEliminar.setOnAction(e -> eliminar());
-
-        Button btnActualizar = new Button("Actualizar");
-        //btnActualizar.setOnAction(e -> actualizar());
-
-        Button btnLeer = new Button("Leer");
-        //btnLeer.setOnAction(e -> leer());
-
-        VBox root = new VBox(10, titulo, btnInsertar, btnEliminar, btnActualizar, btnLeer);
-        root.setPadding(new Insets(20));
-
-        Scene scene = new Scene(root, 300, 200);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Gestión de Pasaportes");
-        primaryStage.show();
-    }
-    private void insertar(Stage primaryStage) {
-        TextField idField = new TextField();
-        idField.setPromptText("Nombre completo");
-
-        TextField fechaField = new TextField();
-        fechaField.setPromptText("Fecha de Expedición");
-
-        TextField paisField = new TextField();
-        paisField.setPromptText("País");
-
-        Button guardarBtn = new Button("Guardar Pasaporte");
-
-        guardarBtn.setOnAction(e -> {
-            Pasaporte pasaporte = new Pasaporte(
-                    idField.getText(),
-                    fechaField.getText(),
-                    paisField.getText()
-            );
-
-            String resultado = repositorio.insertar(pasaporte);
-
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, resultado);
-            alert.showAndWait();
-        });
-        VBox root = new VBox(10, idField, fechaField, paisField, guardarBtn);
-        root.setPadding(new Insets(20));
-        primaryStage.setScene(new Scene(root, 300, 200));
-        primaryStage.setTitle("Gestión de Pasaportes");
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
