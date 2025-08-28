71de73a3b55b2b105d0d285577a6b36a78c64b11
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-08-28 13:20:10 -0500

    Update Operacion.java

--- src/co/poli/edu/actividad1/repositorio/Operacion.java
+++ src/co/poli/edu/actividad1/repositorio/Operacion.java
@@ -2,7 +2,7 @@ package co.poli.edu.actividad1.repositorio;
 
 public interface Operacion<T> {
     String insertar(T entidad);
-    String actualizar(T entidad);
+    String actualizar(String id, T entidad);
     String eliminar(String id);
     T seleccionar(String id);
     java.util.List<T> seleccionarTodos();
