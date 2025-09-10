a97afebc6f10a920e9008836f930192990c4a3dc
AleXamuel <103905900+AleXamuel@users.noreply.github.com>
2025-09-10 17:33:50 -0500

    Update Pasaporte.java

--- src/co/poli/edu/actividad1/modelo/Pasaporte.java
+++ src/co/poli/edu/actividad1/modelo/Pasaporte.java
@@ -1,31 +1,19 @@
 package co.poli.edu.actividad1.modelo;
-import co.poli.edu.actividad1.modelo.Titular;
-import co.poli.edu.actividad1.modelo.Pais;
-public class Pasaporte {
-
-    private String id;
-    private String id_titular;
-    private String fechaEx;
-    private String pais;
-
-    public Pasaporte(String id, String id_titular, String id_pais, String fechaEx) {
-        this.id = id;
-        this.fechaEx = fechaEx;
-        this.pais = id_pais;
-        this.id_titular = id_titular;
-    }
-    public Pasaporte(String Id,String fecha,Titular persona, Pais pais) {
-        id=Id;
-        fechaEx=fecha;
-        this.pais=pais.getCodigo();
-        id_titular=persona.getId();
-    }
+public abstract class Pasaporte {
+    protected String id;
+    protected int id_titular;
+    protected String fechaEx;
+    protected int pais;
     public String toString(){
-        return "["+id+","+id_titular+","+fechaEx+","+pais+"]";
+        return "["+id+","+id_titular+","+pais+","+fechaEx+"]";
     }
-    public String getTitular(){return id_titular;}
+    public int getTitular(){return id_titular;}
     public String getId() { return id; }
     public String getFechaEx() { return fechaEx; }
-    public String getPais() { return pais; }
+    public int getPais() { return pais; }
+    public void setId(String id){this.id=id;}
+    public void setTitular(int id_titular){this.id_titular=id_titular;}
+    public void setFechaEx(String fechaEx){this.fechaEx=fechaEx;}
+    public void setPais(int pais){this.pais=pais;}
 }
 
