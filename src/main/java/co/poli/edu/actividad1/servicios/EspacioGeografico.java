4fe0b542dcb186d80d048a4b1e24c5e6ea487f77
Samuel <barretodiazsamuel@gmail.com>
2025-09-24 19:43:09 -0500

    Metodos Espacio Geografico

--- src/main/java/co/poli/edu/actividad1/servicios/EspacioGeografico.java
+++ src/main/java/co/poli/edu/actividad1/servicios/EspacioGeografico.java
@@ -1,4 +1,21 @@
 package co.poli.edu.actividad1.servicios;
 
-public class EspacioGeografico {
+import java.util.*;
+
+public abstract class EspacioGeografico {
+    protected EspacioGeografico padre;
+    private String nombre;
+    public String getNombre(){
+        return nombre;
+    }
+    public List<EspacioGeografico> getArbolJerarquico(){
+        List<EspacioGeografico> lst=new LinkedList<>();
+        helper(lst,padre);
+        return lst;
+    }
+    private void helper(List<EspacioGeografico>lst,EspacioGeografico root){
+        if(root==null)return;
+        lst.addFirst(root);
+        helper(lst,root.padre);
+    }
 }
