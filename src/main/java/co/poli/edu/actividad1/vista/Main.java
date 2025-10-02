cc0eb2f0c4e0f9b66f56244d4fae41b0cf5fcbd2
Lina <lmmolina@poligran.edu.co>
2025-10-02 12:55:08 -0500

    Implementación de hash

--- src/main/java/co/poli/edu/actividad1/vista/Main.java
+++ src/main/java/co/poli/edu/actividad1/vista/Main.java
@@ -1,5 +1,6 @@
 package co.poli.edu.actividad1.vista;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 import co.poli.edu.actividad1.modelo.*;
@@ -8,10 +9,10 @@ import co.poli.edu.actividad1.servicios.*;
 
 public class Main  {
     private static PasaporteRepositorio repositorio = new PasaporteRepositorio();
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, NoSuchAlgorithmException {
         init();
     }
-    static void init(){
+    static void init() throws NoSuchAlgorithmException {
         /*BuilderPasaporteOrdinario b=new BuilderPasaporteOrdinario();
         b.setIdPasaporte("JUANCHOPOPO");
         b.setFechaPasaporte("05/20/2026");
@@ -20,18 +21,8 @@ public class Main  {
         b.setElemento("1");
         b.setRazonDeViaje("probando 1 2 3");
         eliminar(b.build().getId());*/
-        InterfazTitular t=new AdaptadorTitular(new Titular("101","Edwar Soto","23/05/1979"));
-        System.out.println(t.getDescripcion());
-        t=new DecoradorClase(t,"Clase pobre");
-        System.out.println(t.getDescripcion());
-        t=new DecoradorMascota(t,"Perro Salvavidas");
-        System.out.println(t.getDescripcion());
-        t=new DecoradorClase(t,"Clase mega pobre");
-        System.out.println(t.getDescripcion());
-        t=new DecoradorMascota(t,"Perro Guardian");
-        System.out.println(t.getDescripcion());
-        t=new DecoradorClase(t,"Clase pobre");
-        System.out.println(t.getDescripcion());
+        ElementoDeSeguridad e=new Blockchainhash(new Titular("101","lina","ayer"));
+        System.out.println(e);
     }
     static String insertar(Pasaporte pasaporte) {return repositorio.insertar(pasaporte);}
     static String actualizar(String codigo,Pasaporte pasaporte) {return repositorio.actualizar(codigo,pasaporte);}
