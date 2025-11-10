fcc90e2dcaffae89b77e01f871068f9fc79ebd5f
Samuel <barretodiazsamuel@gmail.com>
2025-11-09 20:50:44 -0500

    Command

---
+++ src/main/java/co/poli/edu/actividad1/servicios/ConsuladoGringo.java
@@ -0,0 +1,10 @@
+package co.poli.edu.actividad1.servicios;
+
+public class ConsuladoGringo {
+    public String aceptar(){
+        return "¡Feliciataciones, su visa fue aprobada!";
+    }
+    public String negar(){
+        return "¡Por desgracia su visa no fue aceptada!";
+    }
+}
